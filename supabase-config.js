cd56d94927e607c508d3dc18938a870a74175dc5
artemchik200307-bit <artemchik.2003.07@gmail.com>
2026-07-17 15:45:18 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,16 @@
+const SUPABASE_URL = "https://vsfwbygmywhzkuiptulc.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  sb_publishable_yVxw8Ipx2s8YLB2b3aCjqw_K0wLTSlU;
+
+window.fastbootSupabase = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
